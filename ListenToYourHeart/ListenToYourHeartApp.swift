751dc81515fda130e42f7e237dfa4d56c10957f5
AndiAlexandropoulos <andiamarbeiten@gmail.com>
2023-01-10 13:12:33 +0100

    Initial Commit

---
+++ ListenToYourHeart/ListenToYourHeartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListenToYourHeartApp.swift
+//  ListenToYourHeart
+//
+//  Created by andi a on 10.01.23.
+//
+
+import SwiftUI
+
+@main
+struct ListenToYourHeartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
